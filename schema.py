e544d6a028e896ae9b72ffd22b4ffdea4f0b57f4
Gordon Hamilton <gordon@Gordons-MacBook-Air.local>
2022-01-06 15:55:29 -0500

    scehma page working

--- schema.py
+++ schema.py
@@ -1,48 +1,8 @@
 from mongoengine import connect, Document, FileField, StringField, FloatField, IntField
 
-connect("imageRepo")
-
-class Image(Document):
-    """
-    MongoDB Document for the image Collection.
-
-    title: string -- title of the image
-    description: string -- description of the image
-    photo: GridFS Object -- image stored in bytes, managed with GridFS
-    """
-    
-    title = StringField(required=True)
-    description = StringField(required=True)
-    photo = FileField(required=True)
-    meta = {"strict": False}
-
-def addImage(filepath, title, description):
-    """
-    Creates image in the MongoDB database based on parameters
-
-    filepath: string -- path to the file of image 
-    title: string -- title of the image
-    description: string -- description of the image
-    """
-    image = Image(title=title)
-    image.description = description
-    fileHandle = open(filepath, "rb")
-    image.photo.put(fileHandle, filename=filepath)
-    image.save()
-
-if __name__ == "__main__":
-    # instantiate variable to test the add image function
-    filepath = "Dog"
-    title = "Dog in sweater"
-    description = "Dog.jpg"
-
-    # run the addImage function
-    # addImage(filepath, title, description)
-
-# ========================
 connect("inventory")
 
-class item(Document):
+class Item(Document):
     """
     MongoDB Document for the inventory item Collection.
 
@@ -52,17 +12,16 @@ class item(Document):
     Quantity int -- amount of item
     """
     
-    Name = StringField(required=True)
+    name = StringField(required=True)
     description = StringField(required=True)
     price = FloatField(required=True)
     quantity = IntField(required=True)
-    meta = {"strict": False}
 
 def addItem(name, description, price, quantity):
     """
     Creates item in the MongoDB database based on parameters
 
-    Name: string -- name of the item
+    name: string -- name of the item
     Description: string -- description of the item
     Price: float -- price of item
     Quantity int -- amount of item
@@ -80,5 +39,5 @@ if __name__ == "__main__":
     price = 123.45
     quantity = 5
 
-    # run the addImage function
+    # run the addItem function to populate db
     addItem(name, description, price, quantity)
